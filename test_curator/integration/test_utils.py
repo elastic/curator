7feae7f8011ea39c4bcd75f72273b2b7da2b1b13
Honza Král <honza.kral@gmail.com>
2014-02-18 15:30:37 +0100

    Tests for _bloom_index

--- test_curator/integration/test_utils.py
+++ test_curator/integration/test_utils.py
@@ -40,3 +40,20 @@ class TestDeleteIndex(CuratorTestCase):
         curator._delete_index(self.client, 'test_index')
         self.assertFalse(self.client.indices.exists('test_index'))
 
+class TestBloomIndex(CuratorTestCase):
+    def test_bloom_filter_will_be_disabled(self):
+        self.create_index('test_index')
+        curator._bloom_index(self.client, 'test_index')
+
+        settings = self.client.indices.get_settings(index='test_index')
+        self.assertEquals('false', settings['test_index']['settings']['index']['codec']['bloom']['load'])
+
+    def test_closed_index_will_be_skipped(self):
+        self.create_index('test_index')
+        self.client.indices.close(index='test_index')
+        self.assertTrue(curator._bloom_index(self.client, 'test_index'))
+        index_metadata = self.client.cluster.state(
+            index='test_index',
+            metric='metadata',
+        )
+        self.assertEquals('close', index_metadata['metadata']['indices']['test_index']['state'])
