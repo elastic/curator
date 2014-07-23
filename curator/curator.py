4f394942d9a935cf96998296e22899bf280dcb54
Aaron Mildenstein <aaron@mildensteins.com>
2014-07-23 17:58:43 -0500

    Bump version to 1.2.0-dev Push logging changes made in curator to es_repo_mgr

--- curator/curator.py
+++ curator/curator.py
@@ -18,7 +18,7 @@ except ImportError:
         def emit(self, record):
             pass
 
-__version__ = '1.1.4-dev'
+__version__ = '1.2.0-dev'
 
 # Elasticsearch versions supported
 version_max  = (2, 0, 0)
