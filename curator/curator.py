0e52c20e70aad2a2e29845f5186809a8f8b102bb
Aaron Mildenstein <aaron@mildensteins.com>
2014-07-29 17:10:46 -0500

    Version bump for release 1.2.2
    Updated CHANGELOG

--- curator/curator.py
+++ curator/curator.py
@@ -18,7 +18,7 @@ except ImportError:
         def emit(self, record):
             pass
 
-__version__ = '1.2.2-dev'
+__version__ = '1.2.2'
 
 # Elasticsearch versions supported
 version_max  = (2, 0, 0)
