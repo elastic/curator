f590ea2238408121ff0c8605970eb25f2a4e9ad4
Aaron Mildenstein <aaron@mildensteins.com>
2014-08-05 16:58:54 -0600

    Getting started on a 2.0 branch
    Fixing VERSION

--- curator/curator.py
+++ curator/curator.py
@@ -18,7 +18,7 @@ except ImportError:
         def emit(self, record):
             pass
 
-__version__ = '1.3.0-dev'
+__version__ = '2.0.0-dev'
 
 # Elasticsearch versions supported
 version_max  = (2, 0, 0)
