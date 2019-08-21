2f66751dd17acc159f8889f1f15056dc6ae7bc5c
Andrew Ballman <andrew.ballman@checkr.com>
2019-08-21 10:39:56 -0700

    Set interval to 1h from 3h for indices

--- run_continuously.sh
+++ run_continuously.sh
@@ -4,6 +4,6 @@ while true
 do
   echo "Starting curator"
   /usr/local/bin/curator --config $CONFIG_FILE $ACTION_FILE
-  echo "finished cleaning indexes..sleeping for 3h"
-  sleep 3h
+  echo "finished cleaning indexes..sleeping for 1h"
+  sleep 1h
 done
