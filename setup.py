79ad10651d1459cb28a58b75e950468fb7432d87
Tero Saarni <tero.saarni@est.tech>
2021-04-15 22:29:03 +0300

    Version bump to pyyaml 5.4.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_install_requires():
     res.append('boto3>=1.9.142')
     res.append('requests_aws4auth>=0.9')
     res.append('click>=6.7,<7.0')
-    res.append('pyyaml==3.13')
+    res.append('pyyaml==5.4.1')
     res.append('voluptuous>=0.9.3')
     res.append('certifi>=2019.9.11')
     res.append('six>=1.11.0')
