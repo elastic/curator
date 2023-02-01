e68390917cb457edac6f9e765b50ce3e480bf78b
Aaron Mildenstein <aaron@mildensteins.com>
2023-01-31 22:24:56 -0700

    Add better settings for doc build

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphin
 
 intersphinx_mapping = {
 	'python': ('https://docs.python.org/3.11', None),
-	'elasticsearch': ('http://elasticsearch-py.readthedocs.io/en/6.8.2', None),
+	'elasticsearch6': ('http://elasticsearch-py.readthedocs.io/en/6.8.2', None),
 }
 
 autoclass_content = "both"
@@ -156,7 +156,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
