aed00dfef08e3cbeae769c4f4a6f234a4fc96b5c
rene-dekker <rene@tigera.io>
2023-06-02 11:04:51 -0700

    Update cx_freeze params

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
   executables=[
-    Executable("run_curator.py", base=base, targetName="curator"),
-    Executable("run_singleton.py", base=base, targetName="curator_cli"),
-    Executable("run_es_repo_mgr.py", base=base, targetName="es_repo_mgr"),
+    Executable("run_curator.py", base=base, target_name="curator"),
+    Executable("run_singleton.py", base=base, target_name="curator_cli"),
+    Executable("run_es_repo_mgr.py", base=base, target_name="es_repo_mgr"),
   ]
 )
