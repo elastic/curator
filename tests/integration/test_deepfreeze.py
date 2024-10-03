355fba63da6c5c924d5cc453e9c766d0bd09eb39
Bret Wortman <bret.wortman@damascusgrp.com>
2024-10-03 07:31:51 -0400

    First integration test started
    Kind of lost about how to approach this

---
+++ tests/integration/test_deepfreeze.py
@@ -0,0 +1,8 @@
+"""Deepfreeze integration tests"""
+
+# pylint: disable=missing-function-docstring, missing-class-docstring, line-too-long
+
+from . import testvars
+from . import CuratorTestCase
+import pytest
+from unittest.case import SkipTest
