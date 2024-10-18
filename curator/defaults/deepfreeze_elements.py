a65e5c710eda425469c6082e9be5639922bd7bd8
Bret Wortman <bret@thewortmans.org>
2024-10-18 07:42:23 -0400

    Updated Schema
    Added defaults and made most schema elements optional

---
+++ curator/defaults/deepfreeze_elements.py
@@ -0,0 +1,91 @@
+"""Deepfreeze element schema definitions
+
+All member functions return a :class:`voluptuous.schema_builder.Schema` object
+"""
+
+from voluptuous import All, Any, Coerce, Optional, Range, Required
+
+# pylint: disable=unused-argument, line-too-long
+
+
+def repo_name_prefix():
+    """
+    Return a :class:`voluptuous.schema_builder.Schema` object for `repo_name_prefix`
+    """
+    return {Optional("repo_name_prefix"): All(Any(str), default="deepfreeze-")}
+
+
+def bucket_name_prefix():
+    """
+    Return a :class:`voluptuous.schema_builder.Schema` object for `bucket_name_prefix`
+    """
+    return {Optional("bucket_name_prefix"): All(Any(str), default="deepfreeze-")}
+
+
+def base_path():
+    """
+    Return a :class:`voluptuous.schema_builder.Schema` object for `base_path`
+    """
+    return {Optional("base_path"): All(Any(str), default="snapshots")}
+
+
+def canned_acl():
+    """
+    Return a :class:`voluptuous.schema_builder.Schema` object for `canned_acl`
+    """
+    return {
+        Optional("canned_acl"): All(
+            Any(
+                "private",
+                "public-read",
+                "public-read-write",
+                "authenticated-read",
+                "log-delivery-write",
+                "bucket-owner-read",
+                "bucket-owner-full-control",
+            ),
+            default="private",
+        )
+    }
+
+
+def storage_class():
+    """
+    Return a :class:`voluptuous.schema_builder.Schema` object for `storage_class`
+    """
+    return {
+        Optional("storage_class"): All(
+            Any(
+                "standard",
+                "reduced_redundancy",
+                "standard_ia",
+                "intelligent_tiering",
+                "onezone_ia",
+            ),
+            default="intelligent_tiering",
+        )
+    }
+
+
+def keep():
+    """
+    This setting is required.
+    Return a :class:`voluptuous.schema_builder.Schema` object for `keep`
+    """
+    return {Required("keep"): All(Coerce(int), Range(min=1))}
+
+
+def year():
+    """
+    This setting is only used to override the current year value.
+    Return a :class:`voluptuous.schema_builder.Schema` object for `year`
+    """
+    return {Optional("year"): All(Coerce(int), Range(min=2000, max=2100))}
+
+
+def month():
+    """
+    This setting is only used to override the current month value.
+    Return a :class:`voluptuous.schema_builder.Schema` object for `month`
+    """
+    return {Optional("month"): All(Coerce(int), Range(min=1, max=12))}
