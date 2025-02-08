85654ce64104bf3c04838475227088608be88e5a
Bret Wortman <bret@thewortmans.org>
2025-02-08 12:46:32 -0500

    Switching to rich for interactive output

--- curator/actions/deepfreeze.py
+++ curator/actions/deepfreeze.py
@@ -4,12 +4,14 @@
 
 import logging
 import re
-import shutil
 import sys
 from dataclasses import dataclass
 from datetime import datetime
 
 from elasticsearch8.exceptions import NotFoundError
+from rich import print
+from rich.console import Console
+from rich.table import Table
 
 from curator.exceptions import ActionError, RepositoryException
 from curator.s3client import S3Client, s3_client_factory
@@ -275,6 +277,9 @@ def create_new_repo(
         )
     except Exception as e:
         loggit.error(e)
+        print(
+            f"[magenta]Error creating repository. Ensure AWS credentials have been added to keystore:[/magenta] {e}"
+        )
         raise ActionError(e)
     #
     # TODO: Gather the reply and parse it to make sure this succeeded
@@ -358,17 +363,6 @@ def decode_date(date_in: str) -> datetime:
         return datetime.fromisoformat(date_in)
     else:
         raise ValueError("Invalid date format")
-
-
-def print_centered(msg: str, fill: str = "-") -> None:
-    """
-    Print a message centered in the terminal window
-
-    :param msg: The message to print
-    :param width: The width of the terminal window
-    """
-    term_width = shutil.get_terminal_size().columns
-    print(msg.center(term_width, fill))
 
 
 class Setup:
@@ -764,42 +758,121 @@ class Status:
     """
 
     def __init__(self, client) -> None:
+        """
+        Setup the status action
+
+        Args:
+            client (elasticsearch): Elasticsearch client object
+        """
         self.loggit = logging.getLogger("curator.actions.deepfreeze")
         self.loggit.debug("Initializing Deepfreeze Status")
         self.settings = get_settings(client)
         self.client = client
+        self.console = Console()
 
     def do_action(self) -> None:
+        """
+        Perform the status action
+        """
         self.loggit.info("Getting status")
         print()
 
         self.do_repositories()
         self.do_buckets()
         self.do_ilm_policies()
+        # self.do_thawsets()
+        self.do_config()
 
-        print_centered("")
-        print()
+    def do_config(self):
+        """
+        Print the configuration settings
+        """
+        table = Table(title="Configuration")
+        table.add_column("Setting", style="cyan")
+        table.add_column("Value", style="magenta")
+
+        table.add_row("Repo Prefix", self.settings.repo_name_prefix)
+        table.add_row("Bucket Prefix", self.settings.bucket_name_prefix)
+        table.add_row("Base Path Prefix", self.settings.base_path_prefix)
+        table.add_row("Canned ACL", self.settings.canned_acl)
+        table.add_row("Storage Class", self.settings.storage_class)
+        table.add_row("Provider", self.settings.provider)
+        table.add_row("Rotate By", self.settings.rotate_by)
+        table.add_row("Style", self.settings.style)
+        table.add_row("Last Suffix", self.settings.last_suffix)
+
+        self.console.print(table)
+
+    def do_thawsets(self):
+        """
+        Print the thawed repositories
+        """
+        table = Table(title="ThawSets")
+        if not self.client.indices.exists(index=STATUS_INDEX):
+            self.loggit.warning("No status index found")
+            return
+        thawsets = self.client.search(index=STATUS_INDEX)
+        for thawset in thawsets:
+            table.add_column(thawset)
+            for repo in thawsets[thawset]:
+                table.add_row(repo)
 
     def do_ilm_policies(self):
-        print_centered("ILM Policies")
+        """
+        Print the ILM policies affected by deepfreeze
+        """
+        table = Table(title="ILM Policies")
+        table.add_column("Policy", style="cyan")
+        table.add_column("Indices", style="magenta")
+        table.add_column("Datastreams", style="magenta")
+        policies = self.client.ilm.get_lifecycle()
+        for policy in policies:
+            # print(f"  {policy}")
+            for phase in policies[policy]["policy"]["phases"]:
+                if (
+                    "searchable_snapshot"
+                    in policies[policy]["policy"]["phases"][phase]["actions"]
+                    and policies[policy]["policy"]["phases"][phase]["actions"][
+                        "searchable_snapshot"
+                    ]["snapshot_repository"]
+                    == f"{self.settings.repo_name_prefix}-{self.settings.last_suffix}"
+                ):
+                    num_indices = len(policies[policy]["in_use_by"]["indices"])
+                    num_datastreams = len(policies[policy]["in_use_by"]["data_streams"])
+                    table.add_row(policy, str(num_indices), str(num_datastreams))
+                    break
+        self.console.print(table)
 
     def do_buckets(self):
-        print_centered("Buckets")
-        print(f"Using provider {self.settings.provider}")
+        """
+        Print the buckets in use by deepfreeze
+        """
+        table = Table(title="Buckets")
+        table.add_column("Provider", style="cyan")
+        table.add_column("Bucket", style="magenta")
+        table.add_column("Base_path", style="magenta")
+
         if self.settings.rotate_by == "bucket":
-            print(
-                f"  Active Bucket: {self.settings.bucket_name_prefix}-{self.settings.last_suffix}"
+            table.add_row(
+                self.settings.provider,
+                f"{self.settings.bucket_name_prefix}-{self.settings.last_suffix}",
+                f"  Active Base Path: {self.settings.base_path_prefix}",
             )
-            print(f"  Active Base Path: {self.settings.base_path_prefix}")
         else:
-            print(f"  Active Bucket: {self.settings.bucket_name_prefix}")
-            print(
-                f"  Active Base Path: {self.settings.base_path_prefix}-{self.settings.last_suffix}"
+            table.add_row(
+                self.settings.provider,
+                f"{self.settings.bucket_name_prefix}",
+                f"  Active Base Path: {self.settings.base_path_prefix}-{self.settings.last_suffix}",
             )
+        self.console.print(table)
 
     def do_repositories(self):
-        print_centered("Repositories")
-        print_centered("Mounted", ".")
+        """
+        Print the repositories in use by deepfreeze
+        """
+        table = Table(title="Mounted Repositories")
+        table.add_column("Repository", style="cyan")
+        table.add_column("Status", style="magenta")
         if not self.client.indices.exists(index=STATUS_INDEX):
             self.loggit.warning("No status index found")
             return
@@ -808,9 +881,13 @@ class Status:
         repolist.sort()
         for repo in repolist:
             if repo == active_repo:
-                print(f"  {repo} MA")
+                table.add_row(repo, "M*")
             else:
-                print(f"  {repo} M")
+                table.add_row(repo, "M")
+        self.console.print(table)
 
     def do_singleton_action(self) -> None:
+        """
+        Dry run makes no sense here, so we're just going to do this either way.
+        """
         self.do_action()
