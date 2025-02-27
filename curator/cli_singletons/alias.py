b31981f479a331bdc5011f125f1b0278647d5398
untergeek <aaron@mildensteins.com>
2025-02-27 15:25:35 -0700

    Updates to cli_singletons
    Mostly cosmetic for PEP8 compliance.

--- curator/cli_singletons/alias.py
+++ curator/cli_singletons/alias.py
@@ -1,8 +1,10 @@
 """Alias Singleton"""
+
 import logging
 import click
 from curator.cli_singletons.object_class import CLIAction
 from curator.cli_singletons.utils import json_to_dict, validate_filter_json
+
 
 @click.command()
 @click.option('--name', type=str, help='Alias name', required=True)
@@ -10,32 +12,34 @@ from curator.cli_singletons.utils import json_to_dict, validate_filter_json
     '--add',
     callback=validate_filter_json,
     help='JSON array of filters selecting indices to ADD to alias',
-    default=None
+    default=None,
 )
 @click.option(
     '--remove',
     callback=validate_filter_json,
     help='JSON array of filters selecting indices to REMOVE from alias',
-    default=None
+    default=None,
 )
 @click.option(
     '--warn_if_no_indices',
     is_flag=True,
-    help='Do not raise exception if there are no actionable indices in add/remove'
+    help='Do not raise exception if there are no actionable indices in add/remove',
 )
 @click.option(
     '--extra_settings',
     help='JSON version of extra_settings (see documentation)',
-    callback=json_to_dict
+    callback=json_to_dict,
 )
 @click.option(
     '--allow_ilm_indices/--no-allow_ilm_indices',
     help='Allow Curator to operate on Index Lifecycle Management monitored indices.',
     default=False,
-    show_default=True
+    show_default=True,
 )
 @click.pass_context
-def alias(ctx, name, add, remove, warn_if_no_indices, extra_settings, allow_ilm_indices):
+def alias(
+    ctx, name, add, remove, warn_if_no_indices, extra_settings, allow_ilm_indices
+):
     """
     Add/Remove Indices to/from Alias
     """
@@ -46,14 +50,17 @@ def alias(ctx, name, add, remove, warn_if_no_indices, extra_settings, allow_ilm_
         'allow_ilm_indices': allow_ilm_indices,
     }
     logger.debug('manual_options %s', manual_options)
-    # ctx.info_name is the name of the function or name specified in @click.command decorator
+    # ctx.info_name is the name of the function or name specified in
+    # @click.command decorator
     ignore_empty_list = warn_if_no_indices
     action = CLIAction(
         ctx.info_name,
         ctx.obj['configdict'],
         manual_options,
-        [], # filter_list is empty in our case
+        [],  # filter_list is empty in our case
         ignore_empty_list,
-        add=add, remove=remove, warn_if_no_indices=warn_if_no_indices, # alias specific kwargs
+        add=add,
+        remove=remove,
+        warn_if_no_indices=warn_if_no_indices,  # alias specific kwargs
     )
     action.do_singleton_action(dry_run=ctx.obj['dry_run'])
