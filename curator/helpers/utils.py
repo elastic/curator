139c56221abd15ad6b17ec6c7cad7e1156d40eb7
untergeek <aaron@mildensteins.com>
2025-02-27 15:37:55 -0700

    Improvements to curator/helpers/utils.py
    There was a shortcoming in the snapshot restore action class. It was relying on the user to
    provide the exact index names to restore. The restore API call to Elasticsearch allows for
    multi-target syntax to select and de-select indices by comma-separated patterns. While not
    expressly allowed, it was possible to use. The problem was that indices could not be properly
    matched and verified if patterns rather than exact names were used. Three functions were added
    to ``helpers.utils`` to make this work properly: ``multitarget_match``, ``multitarget_fix``,
    and ``regex_loop``.
    
    The ``Restore`` class now calls ``multitarget_match`` in the ``_get_expected_output`` method
    to get the names from the snapshot list object. This now works with patterns, and the tests
    have been updated to ensure this.

--- curator/helpers/utils.py
+++ curator/helpers/utils.py
@@ -2,14 +2,20 @@
 
 The kind that don't fit in testers, getters, date_ops, or converters
 """
+
+import re
 import logging
 from es_client.helpers.utils import ensure_list
 from curator.exceptions import FailedExecution
 
+logger = logging.getLogger(__name__)
+
+
 def chunk_index_list(indices):
     """
     This utility chunks very large index lists into 3KB chunks.
-    It measures the size as a csv string, then converts back into a list for the return value.
+    It measures the size as a csv string, then converts back into a list for the
+    return value.
 
     :param indices: The list of indices
 
@@ -32,10 +38,11 @@ def chunk_index_list(indices):
     chunks.append(chunk.split(','))
     return chunks
 
+
 def report_failure(exception):
     """
-    Raise a :py:exc:`~.curator.exceptions.FailedExecution` exception and include the original error
-    message.
+    Raise a :py:exc:`~.curator.exceptions.FailedExecution` exception and include
+    the original error message.
 
     :param exception: The upstream exception.
 
@@ -44,9 +51,10 @@ def report_failure(exception):
     :rtype: None
     """
     raise FailedExecution(
-        f'Exception encountered.  Rerun with loglevel DEBUG and/or check Elasticsearch logs for'
-        f'more information. Exception: {exception}'
+        f'Exception encountered.  Rerun with loglevel DEBUG and/or check '
+        f'Elasticsearch logs for more information. Exception: {exception}'
     )
+
 
 def show_dry_run(ilo, action, **kwargs):
     """
@@ -63,9 +71,10 @@ def show_dry_run(ilo, action, **kwargs):
 
     :rtype: None
     """
-    logger = logging.getLogger(__name__)
     logger.info('DRY-RUN MODE.  No changes will be made.')
-    msg = f'(CLOSED) indices may be shown that may not be acted on by action "{action}".'
+    msg = (
+        f'(CLOSED) indices may be shown that may not be acted on by action "{action}".'
+    )
     logger.info(msg)
     indices = sorted(ilo.indices)
     for idx in indices:
@@ -79,6 +88,7 @@ def show_dry_run(ilo, action, **kwargs):
         msg = f'DRY-RUN: {action}: {idx}{var} with arguments: {kwargs}'
         logger.info(msg)
 
+
 def to_csv(indices):
     """
     :param indices: A list of indices to act on, or a single value, which could be
@@ -86,10 +96,124 @@ def to_csv(indices):
 
     :type indices: list
 
-    :returns: A csv string from a list of indices, or a single value if only one value is present
+    :returns: A csv string from a list of indices, or a single value if only one
+        value is present
     :rtype: str
     """
-    indices = ensure_list(indices) # in case of a single value passed
+    indices = ensure_list(indices)  # in case of a single value passed
     if indices:
         return ','.join(sorted(indices))
     return None
+
+
+def multitarget_fix(pattern: str) -> str:
+    """
+    If pattern only has '-' prefixed entries (excludes)
+    prepend a wildcard to pattern
+
+    :param pattern: The Elasticsearch multi-target syntax pattern
+    :type pattern: str
+
+    :returns: The pattern, possibly with a wildcard prepended
+    :rtype: str
+    """
+    # Split pattern into elements
+    elements = pattern.split(',')
+    # Initialize positive and negative lists
+    positives = []
+    negatives = []
+    # Loop through elements and sort them into positive and negative lists
+    for element in elements:
+        if element.startswith('-'):
+            negatives.append(element)
+        else:
+            positives.append(element)
+    # If there are no elements at all, return a wildcard
+    if len(positives) == 0 and len(negatives) == 0:
+        print("No elements in pattern. Returning '*'")
+        return '*'
+    # If there are no positive elements, but there are negative elements,
+    # add a wildcard to the beginning of the pattern
+    if len(positives) == 0 and len(negatives) > 0:
+        logger.debug(
+            "Only negative elements in pattern. Adding '*' so there's something "
+            " to remove"
+        )
+        return '*,' + pattern
+    # Otherwise, return the original pattern
+    return pattern
+
+
+def regex_loop(matchstr: str, indices: list) -> list:
+    """
+    Loop through indices
+    Match against matchstr
+    return matches
+
+    :param matchstr: The Python regex pattern to match against
+    :param indices: The list of indices to match against
+    :type matchstr: str
+    :type indices: list
+    :returns: The list of matching indices
+    :rtype: list
+    """
+    retval = []
+    for idx in indices:
+        if re.match(matchstr, idx):
+            retval.append(idx)
+    return retval
+
+
+def multitarget_match(pattern: str, index_list: list) -> list:
+    """
+    Convert Elasticsearch multi-target syntax ``pattern`` into Python regex
+    patterns. Match against ``index_list`` and return the list of matches while
+    excluding any negative matches.
+
+    :param pattern: The Elasticsearch multi-target syntax pattern
+    :param index_list: The list of indices to match against
+    :type pattern: str
+    :type index_list: list
+
+    :returns: The final resulting list of indices
+    :rtype: list
+    """
+    retval = []
+    excluded = []
+    includes = []
+    excludes = []
+    logger.debug('Multi-target syntax pattern: %s', pattern)
+    elements = multitarget_fix(pattern).split(',')
+    logger.debug('Individual elements of pattern: %s', elements)
+    for element in elements:
+        # Exclude elements are prefixed with '-'
+        exclude = element.startswith('-')
+        # Any index prepended with a . is probably a hidden index, but
+        # we need to escape the . for regex to treat it as a literal
+        matchstr = element.replace('.', '\\.')
+        # Replace Elasticsearch wildcard * with .* for Python regex
+        matchstr = matchstr.replace('*', '.*')
+        # logger.debug('matchstr: %s', matchstr)
+        # If we have a wildcard in the element and it is not an exclude,
+        # add the output of regex_loop to `includes`
+        if '*' in element and not exclude:
+            includes += regex_loop(matchstr, index_list)
+        # If it is an exclude pattern, add the output of regex_loop to `excludes`
+        # Remove the `-` from the matchstr ([1:]) before sending.
+        if exclude:
+            excludes += regex_loop(matchstr[1:], index_list)
+    # Create a unique list of indices to loop through
+    for idx in list(set(includes)):
+        # If idx is not in the unique list of excludes, add it to retval
+        if idx not in list(set(excludes)):
+            retval.append(idx)
+        else:
+            # Otherwise, add it to the excludes
+            excluded.append(idx)
+    # Sort the lists alphabetically
+    retval.sort()
+    excluded.sort()
+    # Log the results
+    logger.debug('Included indices: %s', retval)
+    logger.debug('Excluded indices: %s', excluded)
+    return retval
