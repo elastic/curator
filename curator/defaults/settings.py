df9d584b4228f913a001d101856961ae2ea119b0
untergeek <aaron@mildensteins.com>
2025-02-27 15:32:13 -0700

    Updates to default settings
    Adding the VERSION_MIN, VERSION_MAX, and multi-target search pattern defaults to filter out system indices
    
    ```diff
    @@ -11,6 +11,12 @@ CLICK_DRYRUN = {
         'dry-run': {'help': 'Do not perform any changes.', 'is_flag': True},
     }
     DATA_NODE_ROLES = ['data', 'data_content', 'data_hot', 'data_warm']
    +EXCLUDE_SYSTEM = (
    +    '-.kibana*,-.security*,-.watche*,-.triggered_watche*,-.watcher-history*,'
    +    '-.ml*,-.geoip_databases*,-.logstas*,-.tasks*'
    +)
    +VERSION_MIN = (7, 14, 0)
    +VERSION_MAX = (8, 99, 99)
    ```

--- curator/defaults/settings.py
+++ curator/defaults/settings.py
@@ -11,6 +11,12 @@ CLICK_DRYRUN = {
     'dry-run': {'help': 'Do not perform any changes.', 'is_flag': True},
 }
 DATA_NODE_ROLES = ['data', 'data_content', 'data_hot', 'data_warm']
+EXCLUDE_SYSTEM = (
+    '-.kibana*,-.security*,-.watche*,-.triggered_watche*,-.watcher-history*,'
+    '-.ml*,-.geoip_databases*,-.logstas*,-.tasks*'
+)
+VERSION_MIN = (7, 14, 0)
+VERSION_MAX = (8, 99, 99)
 
 # Click specifics
 
