29840f5bc3831b56afe703965ea98111d3b261fb
Bret Wortman <bret@thewortmans.org>
2025-05-14 08:36:54 -0700

    Updated to work with nginx reverse proxy

--- reset-ilm.sh
+++ reset-ilm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl -sku bret:2xqT2IO1OQ%tfMHP -X PUT -H "Content-Type: application/json" -d '{"policy":{"phases":{"hot":{"min_age":"0ms","actions":{"rollover":{"max_age":"1m","max_primary_shard_size":"40gb"},"set_priority":{"priority":100}}},"frozen":{"min_age":"5m","actions":{"searchable_snapshot":{"snapshot_repository":"df-eah-test-000001","force_merge_index":true}}},"delete":{"min_age":"10m","actions":{"delete":{"delete_searchable_snapshot":false}}}}}}' "https://192.168.10.31:9200/_ilm/policy/deepfreeze-policy"
+ELASTICSEARCH_HOST="elasticsearch.bwortman.us"
+curl -sku bret:2xqT2IO1OQ%tfMHP -X PUT -H "Content-Type: application/json" -d '{"policy":{"phases":{"hot":{"min_age":"0ms","actions":{"rollover":{"max_age":"1m","max_primary_shard_size":"40gb"},"set_priority":{"priority":100}}},"frozen":{"min_age":"5m","actions":{"searchable_snapshot":{"snapshot_repository":"df-eah-test-000001","force_merge_index":true}}},"delete":{"min_age":"10m","actions":{"delete":{"delete_searchable_snapshot":false}}}}}}' "https://$ELASTICSEARCH_HOST/_ilm/policy/deepfreeze-policy"
 echo
