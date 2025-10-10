ebb8cdfa01069c8c97f84e1f26fbcaa73213f3d2
Bret Wortman <bret@thewortmans.org>
2025-10-10 06:25:40 -0400

    Removing files used for local testing

--- reset-ilm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ELASTICSEARCH_HOST="elasticsearch.bwortman.us"
-curl -sku bret:2xqT2IO1OQ%tfMHP -X PUT -H "Content-Type: application/json" -d '{"policy":{"phases":{"hot":{"min_age":"0ms","actions":{"rollover":{"max_age":"1m","max_primary_shard_size":"40gb"},"set_priority":{"priority":100}}},"frozen":{"min_age":"5m","actions":{"searchable_snapshot":{"snapshot_repository":"df-eah-test-000001","force_merge_index":true}}},"delete":{"min_age":"10m","actions":{"delete":{"delete_searchable_snapshot":false}}}}}}' "https://$ELASTICSEARCH_HOST/_ilm/policy/deepfreeze-policy"
-echo
